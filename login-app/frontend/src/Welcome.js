f2537345055c1b522575f4cf0afcfe214926bcd2
Rishikesh <111395350+rashorge453@users.noreply.github.com>
2022-09-18 00:29:41 -0400

    new change2

--- login-app/frontend/src/Welcome.js
+++ login-app/frontend/src/Welcome.js
@@ -3,6 +3,6 @@ import {Link} from "react-router-dom"
 import SignUp from "./SignUp"
 
 function Welcome(){
-   return <h1>Welcome yde!</h1>
+   return <h1>Welcome Rishi!</h1>
 }
 export default Welcome
